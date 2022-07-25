9ef5341e106f175a26fddb3db9a8dd8608e06a5f
Bret Kitchell <brittonashford@gmail.com>
2022-07-24 21:30:34 -0500

    basic form, state, handlers, and validation

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,75 @@
+import { useFormik } from "formik";
+
+//validation function to pass into useFormik(). 
+//errors object that gets returned must have keys that match the values/initialValues.
+const validate = values => {
+    const errors = {};
+    if(!values.firstname){
+        errors.firstName = 'Required';
+    } else if (values.firstName.length() > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if(!values.lastName){
+        errors.lastname = 'Required';
+    } else if (values.lastName.length() > 20) {
+        errors.lastName = 'Must be 20 characters or less'
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    return errors;
+};
+
+const SignUpForm = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label hmlFor="firstName">First Name</label>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        onChange={formik.onChange}
+        value={formik.values.lastName}
+      />
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
