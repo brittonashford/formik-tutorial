e9b9bcf26cdaf48eb8b110df0f5d553e2bebbd56
Bret Kitchell <brittonashford@gmail.com>
2022-07-24 21:36:21 -0500

    added error message ternaries

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,38 +1,38 @@
 import { useFormik } from "formik";
 
-//validation function to pass into useFormik(). 
+//validation function to pass into useFormik().
 //errors object that gets returned must have keys that match the values/initialValues.
-const validate = values => {
-    const errors = {};
-    if(!values.firstname){
-        errors.firstName = 'Required';
-    } else if (values.firstName.length() > 15) {
-        errors.firstName = 'Must be 15 characters or less';
-    }
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstname) {
+    errors.firstName = "Required";
+  } else if (values.firstName.length() > 15) {
+    errors.firstName = "Must be 15 characters or less";
+  }
 
-    if(!values.lastName){
-        errors.lastname = 'Required';
-    } else if (values.lastName.length() > 20) {
-        errors.lastName = 'Must be 20 characters or less'
-    }
+  if (!values.lastName) {
+    errors.lastname = "Required";
+  } else if (values.lastName.length() > 20) {
+    errors.lastName = "Must be 20 characters or less";
+  }
 
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
 
-    return errors;
+  return errors;
 };
 
 const SignUpForm = () => {
-
   const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
       email: "",
     },
+    //validate will be called when values change or fields are blurred
     validate,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -49,6 +49,7 @@ const SignUpForm = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
       <label htmlFor="lastName">Last Name</label>
       <input
@@ -58,6 +59,7 @@ const SignUpForm = () => {
         onChange={formik.onChange}
         value={formik.values.lastName}
       />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
       <label htmlFor="email">Email Address</label>
       <input
@@ -67,6 +69,7 @@ const SignUpForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <button type="submit">Submit</button>
     </form>
   );
